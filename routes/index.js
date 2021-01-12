6676a9b0833d4acf378c792aa1318f66983c861e
Joshua <j-andriason@live.fr>
2021-01-12 17:18:03 +0100

    bikeshop

--- routes/index.js
+++ routes/index.js
@@ -23,11 +23,11 @@ var dataBike = [{ nom: "BIK045", url: "/images/bike-1.jpg", prix: "679"},
  { nom: "LIK099", url: "/images/bike-6.jpg", prix: "869"}]
  
  var dataCardBike = [{numero:1, nom: "BIK045", url: "/images/bike-1.jpg", prix: "679",quantité : 1 }, 
-{numero:2, nom: "ZOOK07", url: "/images/bike-2.jpg", prix: "999", quantité : 1 },
- {numero:3, nom: "TITANS", url: "/images/bike-3.jpg", prix: "799", quantité: 1}, 
- {numero:4, nom: "CEWO", url: " /images/bike-4.jpg", prix: "1300", quantité: 1},
- {numero:5, nom: "AMIG39", url: "/images/bike-5.jpg", prix: "479", quantité: 1}, 
- {numero:6, nom: "LIK099", url: "/images/bike-6.jpg", prix: "869", quantité: 1}]
+{numero:2, nom: "ZOOK07", url: "/images/bike-2.jpg", prix: "999", quantité : 2 },
+ {numero:3, nom: "TITANS", url: "/images/bike-3.jpg", prix: "799", quantité: 3}, 
+ {numero:4, nom: "CEWO", url: " /images/bike-4.jpg", prix: "1300", quantité: 2},
+ {numero:5, nom: "AMIG39", url: "/images/bike-5.jpg", prix: "479", quantité: 2}, 
+ {numero:6, nom: "LIK099", url: "/images/bike-6.jpg", prix: "869", quantité: 4}]
 
 
 router.get('/shop', function(req, res, next) {
